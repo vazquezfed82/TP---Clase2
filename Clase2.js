cf84137d77177ce1c3ffc63dcc7b4c9c622e6d0b
Federico Vazquez <federicoarielvazquez@gmail.com>
2022-03-21 21:33:10 -0300

    Array

--- Clase2.js
+++ Clase2.js
@@ -1,3 +1,16 @@
 const buscar = () => {
     console.log("Se ejecuta buscar")
+
+    const personas = [
+        { id: 1, nombre: "fede", tipo: "humano", edad: 24},
+        { id: 2, nombre: "nico", tipo: "humano", edad: 24},
+        { id: 3, nombre: "fabi", tipo: "humano", edad: 24},
+        { id: 4, nombre: "rocky", tipo: "perro", edad: 4},
+      
+    ]
+
+    console.log(personas)
+
 }
+
+
